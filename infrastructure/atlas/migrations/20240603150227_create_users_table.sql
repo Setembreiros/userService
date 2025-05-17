e46dbe2df603dde2424c431bd41f36e6faa0c1ea
NewHouses <sergio.simons.c@gmail.com>
2025-05-17 21:03:33 +0300

    restoring migrations

--- infrastructure/atlas/migrations/20240603150227_create_users_table.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE userservice.users (
-    id SERIAL PRIMARY KEY,
-    username VARCHAR(255) UNIQUE,
-    email VARCHAR(255) UNIQUE,
-    user_type VARCHAR(2), 
-    region VARCHAR(255)
-);
